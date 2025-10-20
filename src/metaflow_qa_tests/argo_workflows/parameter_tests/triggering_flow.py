5a846a31db678eb3c9907a18f08368435c165f8b
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2025-10-20 16:50:23 +0300

    add tests for json serialized event payload values

--- src/metaflow_qa_tests/argo_workflows/parameter_tests/triggering_flow.py
+++ src/metaflow_qa_tests/argo_workflows/parameter_tests/triggering_flow.py
@@ -1,5 +1,6 @@
 from metaflow import step, FlowSpec
-from payloads import EVENT_NAME, PAYLOADS
+from payloads import EVENT_NAME, PURE_PAYLOADS, JSONSTR_PAYLOADS
+import json
 
 
 class TriggerArgoParamsTest(FlowSpec):
@@ -8,10 +9,14 @@ class TriggerArgoParamsTest(FlowSpec):
         from metaflow.integrations import ArgoEvent
 
         # trigger events
-        for idx, pl in enumerate(PAYLOADS):
+        for idx, pl in enumerate(PURE_PAYLOADS):
             ArgoEvent(EVENT_NAME).publish({"payload_index": idx, **pl})
-        # ArgoEvent("params_test_event").publish({"param_a": "param-d not supplied", "param-b": "custom payload B", "param-c": None},)
-        # ArgoEvent("params_test_event").publish({"param_a": "param-d not supplied", "param-b": "custom payload B", "param-c": None, "param_d": 444},)
+
+        # Test json serialized payload values.
+        # start indexing correctly after the pure_payloads, which are first in the overall PAYLOADS
+        for idx, pl in enumerate(JSONSTR_PAYLOADS, start=len(PURE_PAYLOADS)):
+            serialized_pl = {k: json.dumps(v) for k, v in pl.items()}
+            ArgoEvent(EVENT_NAME).publish({"payload_index": idx, **serialized_pl})
 
         self.next(self.end)
 
